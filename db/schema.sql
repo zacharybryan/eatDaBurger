a47970033b94e98682b36bfc6131cea942392f38
Zach Bryan <zacharybryan@icloud.com>
2020-11-19 13:00:32 -0800

    created burgers table

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(45) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);

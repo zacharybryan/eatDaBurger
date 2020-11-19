e184cb7c475d39b04c6c3b04ba5ec2c07d794196
Zach Bryan <zacharybryan@icloud.com>
2020-11-19 13:04:24 -0800

    created seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (name) VALUES ('BBQ BACON BURGER');
+INSERT INTO burgers (name) VALUES ('CHEESE BURGER');
+INSERT INTO burgers (name, devoured) VALUES ('VEGGIE BURGER', true);
+INSERT INTO burgers (name, devoured) VALUES ('CHICKEN BURGER', true);
+INSERT INTO burgers (name) VALUES ('SOUTHWESTERN BURGER');

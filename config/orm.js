54ec2f5fbe53afde3d487c27fb2200e9a2a01f61
Zach Bryan <zacharybryan@icloud.com>
2020-11-22 15:06:45 -0800

    select all query from burgers

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,17 @@
-var connection = require('./connection');
+const connection = require('./connection');
 
+const orm = {
+    findAll : function() {
+        return new Promise(function(resolve, reject) {
+            const query = "SELECT * FROM burgers;";
+            connection.query(query, function(err, data){
+                if (err)
+                throw err;
+                resolve(data);
+            });
+        });
+    },
+    
 
 
 module.exports = orm;

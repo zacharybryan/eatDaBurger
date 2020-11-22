4f82b0a3a4245f6b93245d6bf4e9c94f46aa35ad
Zach Bryan <zacharybryan@icloud.com>
2020-11-22 15:30:49 -0800

    get function

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,4 +3,11 @@ const burger = require("../models/burger.js");
 
 const router = express.Router();
 
+router.get("/", function(req, res) {
+    burger.findAll().then(function(data){
+        console.log(data);
+        res.render("index", {burgers:data});
+    });
+});
+
 module.exports = router;

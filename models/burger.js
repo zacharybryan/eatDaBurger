93d02e759ae8a10157d51487e3e86513d4e4a022
Zach Bryan <zacharybryan@icloud.com>
2020-11-22 15:19:55 -0800

    find all

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,12 @@
 const orm = require("../config/orm");
 
+const burger = {
+    findAll: function(){
+        return new Promise(function(resolve, reject) {
+            orm.findAll().then(function(data){
+                resolve(data);
+            });
+        });
+    },
+
 module.exports = burger;

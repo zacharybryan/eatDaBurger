8612998a3df3724913b4da64c3031ace7257a928
Zach Bryan <zacharybryan@icloud.com>
2020-11-22 14:04:36 -0800

    Updates to schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(45) NOT NULL,
+    name VARCHAR(45) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );

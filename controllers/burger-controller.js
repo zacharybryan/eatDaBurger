81da2aca78039f88d8f1796d98386b1f59e64968
Zach Bryan <zacharybryan@icloud.com>
2020-12-12 09:22:20 -0800

    servers out

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -13,23 +13,23 @@ router.get("/", function(req, res) {
     });
 });
 
-// router.post("/burgers/create", function(req, res){
-//     burger.create(req.body.name).then(function(data){
-//         res.json(data);
-//     });
-// });
+router.post("/burgers/create", function(req, res){
+    burger.create(req.body.name).then(function(data){
+        res.json(data);
+    });
+});
 
-// router.delete("/api/burgers/:id", function(req, res) {
-//     burger.destroy(req.params.id).then(function(data){
-//         res.send(200).end();
-//     });
-// });
+router.delete("/api/burgers/:id", function(req, res) {
+    burger.destroy(req.params.id).then(function(data){
+        res.send(200).end();
+    });
+});
 
-// router.put("/api/burgers/:id", function(req, res) {
-//     let devoured = req.body.devoured;
-//     burger.update(req.params.id, req.body.devoured).then(function(data){
-//         res.status(200).end();
-//     })
-// });
+router.put("/api/burgers/:id", function(req, res) {
+    let devoured = req.body.devoured;
+    burger.update(req.params.id, req.body.devoured).then(function(data){
+        res.status(200).end();
+    })
+});
 
 module.exports = router;
